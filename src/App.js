c6917b8bd00f7ba64e96b86df213ce5766a31c8b
Shorouk Alalem <66391713+Shorouk-alalem@users.noreply.github.com>
2022-08-30 20:26:37 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import CoursesListPython from './dist/components/CoursesListPython.js'
+
+function  App() {
+    return (
+      <div className="App">
+      <div className="courses-block">
+        <CoursesListPython />
+      </div>
+      </div>
+    );
+
+
+}
+
+export default App;
